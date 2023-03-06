b20de0c63cba01bbcfe3359686e4dc5a4c8e29da
zm-520 <www.rwzan519521@qq.com>
2023-03-06 10:41:42 +0800

    修改1，2，3

--- src/main/java/zm/test/hello.java
+++ src/main/java/zm/test/hello.java
@@ -3,8 +3,8 @@ package zm.test;
 public class hello {
     public static void main(String[] args) {
         System.out.println("java");
-        System.out.println("ES");
-        System.out.println("ES");
-        System.out.println("jb");
+        System.out.println("ES111");
+        System.out.println("ES11");
+        System.out.println("j1");
     }
 }
